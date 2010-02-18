2a257aa00ad1973fa07faff3a1ba7dffd71f8c2d
corey <corey@codenothing.com>
2010-02-17 19:23:34 -0800

    Adding version 1.1

--- jquery.single-ddm.js
+++ jquery.single-ddm.js
@@ -1,55 +1,50 @@
 /**
- * Single Drop Down Menu
- * March 24, 2008
+ * Single Drop Down Menu 1.1
+ * August 22, 2009
  * Corey Hart @ http://www.codenothing.com
- *
- * @timer: milliseconds to keep dropped menus open
- * @parentMO: CSS class to add/remove to parent list on mouseover/mouseout's
- * @childMO: CSS class to add/remove to child list on mouseover/mouseout's
- */ 
+ */
 ;(function($){
  	// Define the function
 	$.fn.singleDropMenu = function(options){
-		// Function Vars
-		var timer; // ID of timeout set
-		var menu; // ID of menu open
-		// Default Settings
-		var settings = $.extend({
-			timer: 500,
-			parentMO: null,
-			childMO: null
-		},options||{});
-
-		// Run Menu
-		this.each(function(){
-			$('li', this).mouseover(function(){
+		return this.each(function(){
+			// Default Settings
+			var $obj = $(this), timer, menu,
+				settings = $.extend({
+					timer: 500,
+					parentMO: 'non-404',
+					childMO: 'non-404'
+				}, options||{}, $.metadata ? $obj.metadata() : {});
+	
+			// Run Menu
+			$obj.children('li').mouseover(function(){
 				// Clear any open menus
 				closemenu();
 				// Open nested list
-				$('a', this).addClass(settings.parentMO).siblings('ul').show();
+				$(this).children('a').addClass(settings.parentMO).siblings('ul').show();
 			}).mouseout(function(){
 				// Prevent auto close
-				menu = this;
-				timer = setTimeout(function(){closemenu()}, settings.timer);
+				menu = $(this);
+				timer = setTimeout(closemenu, settings.timer);
 			});
+
 			// Dropped Menu Highlighting
-			$('li ul li', this).mouseover(function(){
-				if (settings.childMO) $('a', this).addClass(settings.childMO);
-			}).mouseout(function(){
-				if (settings.childMO) $('a', this).removeClass(settings.childMO);
+			$('li > ul > li', $obj).hover(function(){
+				$('a', this).addClass(settings.childMO);
+			}, function(){
+				$('a', this).removeClass(settings.childMO);
 			});
-		});
-
-		// Closes any open menus when mouse click occurs anywhere else on the page
-		$(document).click(function(){closemenu();});
-
-		// Function to close set menu
-		var closemenu = function(){
-			if (menu && timer){
-				$('a', menu).removeClass(settings.parentMO).siblings('ul').hide();
-				clearTimeout(timer);
-				menu = '';
+	
+			// Closes any open menus when mouse click occurs anywhere else on the page
+			$(document).click(closemenu);
+	
+			// Function to close set menu
+			function closemenu(){
+				if (menu && timer){
+					menu.children('a').removeClass(settings.parentMO).siblings('ul').hide();
+					clearTimeout(timer);
+					menu = false;
+				}
 			}
-		}
-	}
+		});
+	};
 })(jQuery);
