26c1dab41f32b49382a599200c1eb3fdb4047d3a
corey <corey@codenothing.com>
2010-02-19 00:55:49 -0800

    Variable Replacement Setup

--- revar.sh
+++ revar.sh
@@ -4,4 +4,4 @@
 grep -ilr --exclude-dir=.git --exclude=revar.sh '[DATE]' * | xargs -i@ sed -i 's/\[DATE\]/February 19, 2010/g' @
 
 # Version Changer
-grep -ilr --exclude-dir=.git --exclude=revar.sh '[VERSION]' * | xargs -i@ sed -i 's/\[DATE\]/1.2/g' @
+grep -ilr --exclude-dir=.git --exclude=revar.sh '[VERSION]' * | xargs -i@ sed -i 's/\[DATE\]/1.3/g' @
